514201ac872d999b3a2731984edb5f06291c4aff
ctokoli <matumasie@gmail.com>
2024-01-12 16:48:57 +0000

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/Documentation
 class Article < ApplicationRecord
   validates :title, presence: true
 end
+# rubocop:enable Style/Documentation
